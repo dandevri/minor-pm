6ad983ee082069254b1d6b6200ba7e2182d0ffc6
Danny de Vries <vriesdan@gmail.com>
2017-03-28 23:03:29 +0200

    :lipstick: Added first HTML render and added styling
    Make it look more like formula 1 woei

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('It has works');
+var express = require('express');
+var request = require('request');
+var dotenv = require('dotenv').config();
+var app = express();
+
+var api_url_driver = process.env.API_URL_DRIVERSTANDINGS;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', function(req, res) {
+  request(api_url_driver, function(error, response, body) {
+    var data = JSON.parse(body);
+    res.render('index.ejs', {driver: data});
+  });
+})
+
+var server = app.listen(1337, function () {
+   console.log('The server is running.')
+})
